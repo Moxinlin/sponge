2e2a3ea624304fe0279f16c96f4439b790349e48
moxinlin <491892011@qq.com>
2022-11-26 22:57:47 +0800

    lab0

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,4 +1,5 @@
 #include "byte_stream.hh"
+#include <iostream>
 
 // Dummy implementation of a flow-controlled in-memory byte stream.
 
@@ -12,42 +13,75 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity) {
+    _buf_cap = capacity > 0 ? capacity : 1;
+    _buffer.resize(_buf_cap);
+    _read_idx = 0;
+    _buf_size = 0;
+    _read_size = 0;
+    _write_size = 0;
+}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    size_t i = 0;
+    while (_buf_size < _buf_cap && i < data.size()) {
+        _buffer[(_read_idx + _buf_size) % _buf_cap] = data[i];
+        _buf_size++;
+        i++;
+        _write_size++;
+    }
+    
+    return i;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    std::string data = "";
+    size_t t = std::min(len, _buf_size);
+    for (size_t i = 0; i < t; ++i) {
+        data.append(&_buffer[(_read_idx + i) % _buf_cap], 1);
+    }
+    return data;
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) {
+    size_t t = std::min(len, _buf_size);
+    _read_idx += t;
+    _buf_size -= t;
+    _read_size += t;
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    std::string data;
+    size_t read_len = 0;
+    while (_buf_size > 0 && read_len < len) {
+        data.append(&_buffer[_read_idx], 1);
+        _read_idx = (_read_idx + 1) % _buf_cap;
+        _buf_size--;
+        _read_size++;
+        read_len++;
+    }
+    return data;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() { _input_end = true;}
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return _input_end; }
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const { return _buf_size; }
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const { 
+    return _buf_size == 0; 
+}
 
-bool ByteStream::eof() const { return false; }
+bool ByteStream::eof() const { return _input_end && _buf_size == 0; }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return _write_size; }
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return _read_size; }
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const { return _buf_cap - _buf_size; }
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -2,6 +2,7 @@
 #define SPONGE_LIBSPONGE_BYTE_STREAM_HH
 
 #include <string>
+#include <vector>
 
 //! \brief An in-order byte stream.
 
@@ -11,6 +12,10 @@
 class ByteStream {
   private:
     // Your code here -- add private members as necessary.
+    std::vector<char> _buffer{};
+    size_t _read_idx = 0, _buf_size = 0, _buf_cap = 0;
+    size_t _read_size = 0, _write_size = 0;
+    bool _input_end = false, _eof = false;
 
     // Hint: This doesn't need to be a sophisticated data structure at
     // all, but if any of your tests are taking longer than a second,
