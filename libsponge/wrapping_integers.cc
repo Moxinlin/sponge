e5ddcea4a48c5f2a33ba6eb449a972d2b132cb05
moxinlin <491892011@qq.com>
2022-12-12 22:21:00 +0800

    wrapping_integers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#include <iostream>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -14,8 +15,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    WrappingInt32 seqno = isn + n;
+    return seqno;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +30,25 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t offset = static_cast<uint64_t>(n - isn);
+    uint64_t INT32_RANGE = (1UL << 32);
+    uint64_t wrap_num = checkpoint / INT32_RANGE;
+    uint64_t max_wrap_num = (1UL << 32) - 1;
+
+    uint64_t left_seqno = (wrap_num > 1 ? wrap_num-1: wrap_num) * INT32_RANGE + offset;
+    uint64_t mid_seqno = wrap_num * INT32_RANGE + offset;
+    uint64_t right_seqno =  (wrap_num + 1 > max_wrap_num ? max_wrap_num : wrap_num + 1) * INT32_RANGE + offset;
+    
+    uint64_t left_diff = (checkpoint > left_seqno ? checkpoint - left_seqno : left_seqno - checkpoint);
+    uint64_t mid_diff = (checkpoint > mid_seqno ? checkpoint - mid_seqno : mid_seqno - checkpoint);
+    uint64_t right_diff = (checkpoint > right_seqno ? checkpoint - right_seqno : right_seqno - checkpoint);
+
+    uint64_t min_diff = std::min(std::min(left_diff, mid_diff), right_diff);
+    if (min_diff == left_diff) {
+        return left_seqno;
+    }
+    if (min_diff == mid_diff) {
+        return mid_seqno;
+    }
+    return right_seqno;
 }
